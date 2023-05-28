2543a7f56f7c2fb43f4be2527f8a55a6a17dc675
Prachi Bisht <103419553+bisht-prachi@users.noreply.github.com>
2023-05-28 13:31:54 +0530

    Add files via upload

--- antialigners.py
+++ antialigners.py
@@ -15,58 +15,82 @@ np.random.seed(seed=int(time.time()))
 #simulation parameters
 v0       = 1
 dt       = 1
-eta      = 0.5
+eta      = 0
 R        = 10       
-L        = 100
-Nt       = 2000
-N        = 10000
+L        = 200
+Nt       = 5000
+N        = 40000
+T        = 5
+binsize  = 0.1
 plotRealTime = True
 
 def simulate(coord):    
-       # fig = plt.figure(figsize=(4,4), dpi=80)
-       # ax = plt.gca()
-       
-       #run
-       for t in range(Nt):
-           #get x,y,theta
-           x = np.array([[item[0]] for item in coord])
-           y = np.array([[item[1]] for item in coord])
-           theta = np.array([[item[2]] for item in coord])
-           
-           #update x,y coordinates
-           x = (x + v0 * dt * np.cos(theta)) % L
-           y = (y + v0 * dt * np.sin(theta)) % L                      
-                 
-           #update orientation. 
-           #sequential update of orientation
-           for i in range(N):           
-               neighbors = (x-x[i])**2 + (y-y[i])**2 < R**2
-               sx = np.sum(np.cos(theta[neighbors]))
-               sy = np.sum(np.sin(theta[neighbors]))
-               theta[i] = np.arctan2(-sy,-sx)             
+    # fig = plt.figure(figsize=(4,4), dpi=80)
+    # ax = plt.gca()
+    
+    #run
+     #get x,y,theta
+     x = np.array([[item[0]] for item in coord])
+     y = np.array([[item[1]] for item in coord])
+     theta = np.array([[item[2]] for item in coord])    
+     
+     #update x,y coordinates 
+     x = (x + v0 * dt * np.cos(theta)) % L
+     y = (y + v0 * dt * np.sin(theta)) % L      
+                     
+     for i in range(N):
+         #update orientation. 
+         neighbors = (x-x[i])**2 + (y-y[i])**2 < R**2
+         sx = np.sum(np.cos(theta[neighbors]))
+         sy = np.sum(np.sin(theta[neighbors]))
+         theta[i] = np.arctan2(-sy,-sx) + eta*(random.uniform(0,1)-0.5)             
+     
+     #update coordinates
+     for i in range(len(coord)):
+         coord[i][0] = x[i]
+         coord[i][1] = y[i]
+         coord[i][2]  = theta[i]            
+         
+     #shuffle the particle order        
+     np.random.shuffle(coord)          
+         
+     # if plotRealTime or (t == Nt - 1):     
+     #     plt.cla()
+     #     plt.quiver(x, y, np.cos(theta), np.sin(theta), np.arctan2(np.sin(theta), np.cos(theta)), angles='xy', scale_units='xy', scale=0.5, pivot='mid', cmap='hsv_r')
+     #     ax.set(xlim=(0, L), ylim=(0, L))
+     #     ax.set_aspect('equal')	           
+     
             
-           #add randomness
-           theta = theta + eta*(np.random.rand(N,1)-0.5)
-           
-           #update coordinates
-           for i in range(len(coord)):
-               coord[i][0] = x[i]
-               coord[i][1] = y[i]
-               coord[i][2]  = theta[i]            
-               
-           #shuffle the particle order        
-           np.random.shuffle(coord)          
-               
-           # if plotRealTime or (t == Nt - 1):     
-           #     plt.cla()
-           #     plt.quiver(x, y, np.cos(theta), np.sin(theta), np.arctan2(np.sin(theta), np.cos(theta)), angles='xy', scale_units='xy', scale=0.5, pivot='mid', cmap='hsv_r')
-           #     ax.set(xlim=(0, L), ylim=(0, L))
-           #     ax.set_aspect('equal')	           
-           
-               
-       return 0
+     return 0
+ 
+def getthetaDist(coord, thetaDist):    
+    theta = np.array([[item[2]] for item in coord])
+    for i in range(len(theta)):
+        a = (theta[i] + np.pi) % (2 * np.pi)
+        a /= binsize
+        thetaDist[int(a)] += 1
+            
+     
    
-       
+def getvelCorr(coord, velCorr,counter):
+    x = np.array([[item[0]] for item in coord])
+    y = np.array([[item[1]] for item in coord])
+    theta = np.array([[item[2]] for item in coord])
+
+   
+    for i in range(len(theta)):
+    # i = random.randint(0,N-1)
+        for j in range(len(theta)):
+            dx = abs(x[j] - x[i]) 
+            dy = abs(y[j] - y[i])
+            r = np.sqrt(dx**2 + dy**2)
+            if r > np.sqrt(2)*0.5*L: r = np.sqrt(2)*L - r
+            bin = int(r/binsize)
+            counter[bin] += 1
+            velCorr[bin] += (np.cos(theta[i]) * np.cos(theta[j]) + np.sin(theta[i]) * np.sin(theta[j]))
+            
+
+      
 if __name__ == "__main__":
     coord = np.random.rand(N,3)
     for i in range(len(coord)):
@@ -75,3 +99,16 @@ if __name__ == "__main__":
         coord[i][2]  = 2 * np.pi * (coord[i][2] - 0.5)
         
     simulate(coord)
+    x = np.array([[item[0]] for item in coord])
+    y = np.array([[item[1]] for item in coord])
+    theta = np.array([[item[2]] for item in coord])
+
+    fig = plt.figure(figsize=(8,8), dpi=80)
+    ax = plt.gca()
+    plt.title(r'N = {} particles, R = {}, eta = {}. Initial State'.format(N,R,eta))      
+    plt.quiver(x, y, np.cos(theta), np.sin(theta), np.arctan2(np.sin(theta), np.cos(theta)), angles='xy', scale_units='xy', scale=0.5, pivot='mid', cmap='hsv_r')
+    plt.clim(-np.pi,np.pi)
+    cbar = plt.colorbar()
+    cbar.ax.set_ylabel(r'theta', rotation=0)  
+    ax.set(xlim=(0, L), ylim=(0, L))
+    ax.set_aspect('equal')	           
--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 from antialigners import *
-from scipy.stats import gaussian_kde
 
 plt.rcParams['text.usetex'] = True
-plt.rcParams['font.size'] = '12'
+plt.rcParams['font.size'] = '28'
+plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
 
 def main():
     #initialize, x, y, theta
@@ -12,41 +12,45 @@ def main():
         coord[i][1] *= L
         coord[i][2]  = 2 * np.pi * (coord[i][2] - 0.5)
         
+    #plot particle distribution in 2D space: initial state
     x = np.array([item[0] for item in coord])
     y = np.array([item[1] for item in coord])
     theta = np.array([item[2] for item in coord])
-        
-    #plot particle distribution in 2D space: initial state
-    fig1 = plt.figure(figsize=(8,8), dpi=80)
-    ax = fig1.gca()
+    fig = plt.figure(figsize=(10,8))
+    ax = fig.gca()
     ax.set(xlim=(0, L), ylim=(0, L))
-    # ax.set_xlabel(r'x')
-    # ax.set_ylabel(r'y')
-    ax.set_xlabel(r'\textbf{x}')
-    ax.set_ylabel(r'\textbf{y}')
-    ax.set_aspect('equal')	
+    ax.set_xlabel(r'x')
+    ax.set_ylabel(r'y')
+    ax.set_aspect('equal')	    
     plt.title(r'N = {} particles, R = {}, $\eta$ = {}. Initial State'.format(N,R,eta))      
     plt.quiver(x, y, np.cos(theta), np.sin(theta), np.arctan2(np.sin(theta), np.cos(theta)), angles='xy', scale_units='xy', scale=0.5, pivot='mid', cmap='hsv_r')
     plt.clim(-np.pi,np.pi)
     cbar = plt.colorbar()
-    cbar.ax.set_ylabel('orientation', rotation=270)  
-    plt.show()     
-    plt.savefig('antialignersI.png',bbox_inches='tight',dpi=100)
-        
-    #simulate
-    simulate(coord)
+    cbar.ax.set_ylabel(r'\textbf{$\theta$}', rotation=0)      
+    plt.savefig('antialigners_initial_state.eps', format='eps',bbox_inches='tight',dpi=100)
+    plt.show() 
+    plt.close()
     
+    #simulate till steady state
+    for _ in range(Nt): 
+        simulate(coord)
+    
+    #declare empty array to store frequency of data
+    thetaDist = np.zeros(int(7/binsize), dtype=float)
+    
+    for _ in range(T):
+        simulate(coord)
+        getthetaDist(coord, thetaDist)
+        
+    # plot particle distribution in 2D space: steady state
     x = np.array([item[0] for item in coord])
     y = np.array([item[1] for item in coord])
     theta = np.array([item[2] for item in coord])
-    
-    
-    #plot particle distribution in 2D space: steady state
-    fig1 = plt.figure(figsize=(8,8), dpi=80)
-    ax = fig1.gca()
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.gca()
     ax.set(xlim=(0, L), ylim=(0, L))
-    # ax.set_xlabel(r'x')
-    # ax.set_ylabel(r'y')
+    ax.set_xlabel(r'x')
+    ax.set_ylabel(r'y')
     ax.set_xlabel(r'\textbf{x}')
     ax.set_ylabel(r'\textbf{y}')
     ax.set_aspect('equal')	    
@@ -54,36 +58,26 @@ def main():
     plt.quiver(x, y, np.cos(theta), np.sin(theta), np.arctan2(np.sin(theta), np.cos(theta)), angles='xy', scale_units='xy', scale=0.5, pivot='mid', cmap='hsv_r')
     plt.clim(-np.pi,np.pi)
     cbar = plt.colorbar()
-    cbar.ax.set_ylabel('orientation', rotation=270)  
-    plt.show()     
-    plt.savefig('antialignersS.png',bbox_inches='tight',dpi=100)
+    cbar.ax.set_ylabel(r'\textbf{$\theta$}', rotation=0)       
+    plt.savefig('antialigners_steady_stateL50eta0.eps', format = 'eps', bbox_inches='tight',dpi=100)
+    plt.show()  
     
+    with open('thetaDistL100eta0.csv', 'w') as f:
+        for a in range(len(thetaDist)):
+            f.write(f"{a*binsize - np.pi},{thetaDist[a]/(N*T)}\n")
+            
+    #plot theta frequency plot
+    fig1 = plt.figure(figsize=(8,6))
+    ax1 = fig1.gca()
+    ax1.set(xlim=(-3.5, 3.5))
+    ax1.set_xlabel(r'\textbf{$\theta$ (rad)}')
+    ax1.set_ylabel(r'\textbf{P($\theta$) }')
+    plt.plot(np.arange(len(thetaDist))*binsize - np.pi, thetaDist/(N*T), marker = '.')
+    plt.grid()
+    plt.savefig('thetaDist.png', bbox_inches='tight',dpi=100)
+    plt.show()  
     
-    #plot histogram for theta distribution
-    fig2 = plt.figure()
-    ax2 = fig2.gca()        
-    ax2.set_xlabel(r'${\bf{\theta}}$')
-    ax2.set_ylabel(r'\textbf{frequency}')    
-    density = gaussian_kde(theta)
-    t = np.linspace(-4, 4, 1000)
-    density.covariance_factor = lambda : 0.1 #Smoothing parameter
-    density._compute_covariance()
-    plt.title('Histogram for orientation distribution \n N = {} particles, R = {}, $\eta$ = {}'.format(N,R,eta))
-    plt.plot(t, density(t))
-    plt.show()
-    plt.savefig('thetaDist.png',dpi=100)
-    
-    # fig3 = plt.figure()
-    # ax3 = fig3.gca()
-    # counts, bins = np.histogram(theta)
-    # plt.hist(bins[:-1], bins, weights=counts)
-    # ax3.set_xlabel(r'$\theta$')
-    # ax3.set_ylabel(r'frequency')   
-    # plt.title('Histogram for orientation distribution \n N = {} particles, R = {}, $\eta$ = {}'.format(N,R,eta))
-    # plt.show()
-    # plt.savefig('thetaHist.png',dpi=100)
-    
-    
+   
 
 if __name__ == "__main__":
     main()
